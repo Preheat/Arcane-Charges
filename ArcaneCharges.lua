249e454a46ffb3778f9830fc67662d3a178b2c18
Preheat <switzer1337@gmail.com>
2016-05-17 10:37:55 -0500

    Update ArcaneCharges.lua

--- ArcaneCharges.lua
+++ ArcaneCharges.lua
@@ -1,13 +1,12 @@
 function()
     local power = UnitPower("player", SPELL_POWER_ARCANE_CHARGES, true);
     if power > 3 then
-        return ""..power
+        return "|cFFFF00FF"..power
     elseif power > 2 then
-        return ""..power
+        return "|cFFFF44FF"..power
     elseif power > 1 then
-        return ""..power
+        return "|cFFFF88FF"..power
     elseif power > 0 then
-        return ""..power
+        return "|cFFFFFFFF"..power
     end
-    
 end
